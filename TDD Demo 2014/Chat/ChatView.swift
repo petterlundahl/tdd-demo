5bb1dc301af683f6ead422538663ed812eea2eed
Petter Lundahl <petter.lundahl@daresay.co>
2024-12-04 13:45:33 +0100

    Make the chat more pretty

--- TDD Demo 2014/Chat/ChatView.swift
+++ TDD Demo 2014/Chat/ChatView.swift
@@ -58,8 +58,7 @@ private struct ChatMessageView: View {
       switch message.sender {
       case .you:
         Spacer()
-        Text(message.text)
-          .padding()
+        MessageContentView(text: message.text, time: message.time)
           .background(
             RoundedRectangle(cornerRadius: 16)
               .fill(Color.blue)
@@ -70,18 +69,41 @@ private struct ChatMessageView: View {
       case .other(let sender):
         Image(systemName: "person.circle.fill")
           .scaleEffect(1.5)
-        Text(message.text)
-          .padding()
+        MessageContentView(text: message.text, sender: sender, time: message.time)
           .background(
             RoundedRectangle(cornerRadius: 16)
               .fill(Color(UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1.0)))
           )
           .shadow(color: .gray.opacity(0.5), radius: 5, x: 0, y: 2)
-          .font(.title2)
         Spacer()
       }
-      
     }
+  }
+}
+
+private struct MessageContentView: View {
+  private let text: String
+  private let sender: String?
+  private let time: String
+  
+  init(text: String, sender: String? = nil, time: String) {
+    self.text = text
+    self.sender = sender
+    self.time = time
+  }
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      if let sender {
+        Text(sender)
+          .font(.subheadline).bold()
+      }
+      Text(text)
+        .font(.title2)
+      Text(time)
+        .font(.footnote)
+    }
+    .padding()
   }
 }
 
@@ -97,11 +119,11 @@ private struct ChatMessageView: View {
       Message(text: "And to you as well!", sender: .you, state: .sent, time: "14:58"),
     ]),
     .init(header: "31 december", messages: [
-      Message(text: "Happy new year!", sender: .other("Alice"), state: .sent, time: "23:58")
+      Message(text: "Happy new year!", sender: .other("Bob"), state: .sent, time: "23:58")
     ]),
     .init(header: "Today", messages: [
       Message(text: "Hello guys! Do you want to do something fun today like maybe visit an owl sanctuary?", sender: .you, state: .sent, time: "07:15"),
-      Message(text: "For sure! That sounds like an excellent idea!", sender: .other("Bob"), state: .sent, time: "07:16")
+      Message(text: "For sure! That sounds like an excellent idea!", sender: .other("Alice"), state: .sent, time: "07:16")
     ])
   ])
   ChatView(viewModel: .mocked(state: state))
